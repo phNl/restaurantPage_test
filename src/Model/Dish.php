aa618396b5ec7c3dfb8290f8a55ebd7356f3ed01
phNl <86802257+phNl@users.noreply.github.com>
2022-06-25 18:45:25 +1000

    Update Dish.php

--- src/Model/Dish.php
+++ src/Model/Dish.php
@@ -2,18 +2,24 @@
 
 namespace ComposerTest\Model;
 
+use PDO;
+
 class Dish
 {
     private string $name;
     private int $price;
     private int $weight;
     private string $photo;
+    
+    private PDO $db;
 
     public function __construct(string $name, int $price, int $weight) {
         $this->name = $name;
         $this->price = $price;
         $this->weight = $weight;
         $this->photo = '';
+        
+        $this->db = new PDO("mysql:host=198.211.107.171;dbname=restaurant", phnl, 52345234);
     }
 
     /**
@@ -56,5 +62,12 @@ class Dish
         $this->photo = $photo;
     }
 
+    // Active-Record
+    public function Save() : bool
+    {
+        $query = $this->db->prepare("INSERT INTO dish(name, weight, price, photo) 
+            VALUES (:name, :weight, :price, :photo)");
+        return $query->execute(['name' => $this->name, 'weight' => $this->weight, 'price' => $this->price, 'photo' => $this->photo]);
+    }
 
 }
